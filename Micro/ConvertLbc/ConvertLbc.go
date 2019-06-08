5425bf70f2ca0f6fcdcc8d53feddad699bc1991c
bussiere <bussiere@gmail.com>
2019-06-08 19:52:37 +0200

    wip

--- Micro/ConvertLbc/ConvertLbc.go
+++ Micro/ConvertLbc/ConvertLbc.go
@@ -1,29 +1,50 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 	"github.com/lib/pq"
+	"strings"
+	"time"
 )
 
 
 
 
 type RawOffer struct {
-	RawOffer string  `json:"RawOffer"`
+	RawOfferData string  `json:"Raw"`
+	Modified      DateTime   `db:"Modified" json:"Modified"`
+	Created       DateTime   `db:"Created" json:"Created"`
+	Used sql.NullBool `db:"Used"`
+}
+
+type RawOfferLbc struct {
+	RawOfferData string  `json:"RawOffer"`
 	Modified      pq.NullTime    `db:"modified"`
 	Created       pq.NullTime    `db:"created"`
 }
 
 
 
-
 func convertLbc(c *gin.Context) {
 	var rawOffer RawOffer
 	c.BindJSON(&rawOffer)
-	fmt.Println(rawOffer)
+	fmt.Println(rawOffer.Created)
+	var err error
+	s := strings.Split(rawOffer.RawOfferData, `"ads": `)
+
+	s2 := `{ "ads": ` + s[1]
+	result := strings.Replace(s2, `, "status": "ready"}]`, ``, -1)
+	//fmt.Println(result)
+	var rawOfferLbc RawOfferLbc
+	if err = json.Unmarshal([]byte(result), &rawOfferLbc); err != nil {
+		fmt.Println(result)
+		panic(err)
+	}
 
 }
 
---
+++ Micro/ConvertLbc/go.mod
@@ -0,0 +1,10 @@
+module .
+
+go 1.12
+
+require (
+	github.com/gin-contrib/cors v1.3.0 // indirect
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+	gopkg.in/guregu/null.v3 v3.4.0 // indirect
+)
