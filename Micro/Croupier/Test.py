274925161bc088da9efb011e90d0d68b2e44a094
bussiere <bussiere@gmail.com>
2019-07-18 17:48:21 +0200

    wip

--- Micro/Croupier/Test.py
+++ Micro/Croupier/Test.py
@@ -3,3 +3,4 @@ import requests
 
 r = requests.post("http://127.0.0.1:8091/getOfferFromHash/",json={"Geohash":"u09tvnrbtbt3w3st","NbMinuteCar":30,"NbMinuteWalk":30})
 print(r.json())
+print(r.json()["Offers"][0])
